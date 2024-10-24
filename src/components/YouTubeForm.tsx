9d5dc6853c37d3a25eecdc9ab4648bd7df6b14d1
Morgan <fldntjr@naver.com>
2024-10-24 17:18:16 +0900

    study up to 9 chapter(Display Error Message)

---
+++ src/components/YouTubeForm.tsx
@@ -0,0 +1,71 @@
+import { useForm } from "react-hook-form";
+import { DevTool } from "@hookform/devtools";
+
+let renderCount = 0;
+
+type FormValues = {
+  username: string;
+  email: string;
+  channel: string;
+};
+
+export default function YouTubeForm() {
+  const form = useForm<FormValues>();
+  const { register, control, handleSubmit, formState } = form;
+  const { errors } = formState;
+  // const { name, ref, onChange, onBlur } = register("username");
+
+  const onSubmit = (data: FormValues) => {
+    console.log(data);
+    console.log("Form submitted");
+  };
+
+  renderCount++;
+
+  return (
+    <div>
+      <h1>YouTube Form ({renderCount / 2})</h1>
+
+      <form onSubmit={handleSubmit(onSubmit)} noValidate>
+        <div className="form-control">
+          <label htmlFor="username">Username</label>
+          <input
+            type="text"
+            id="username"
+            {...register("username", { required: { value: true, message: "Username is required" } })}
+          />
+          <p className="error">{errors.username?.message}</p>
+        </div>
+
+        <div className="form-control">
+          <label htmlFor="email">E-mail</label>
+          <input
+            type="email"
+            id="email"
+            {...register("email", {
+              pattern: {
+                value: /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/,
+                message: "Invalid email format",
+              },
+            })}
+          />
+          <p className="error">{errors.email?.message}</p>
+        </div>
+
+        <div className="form-control">
+          <label htmlFor="channel">Channel</label>
+          <input
+            type="text"
+            id="channel"
+            {...register("channel", { required: { value: true, message: "Channel is required" } })}
+          />
+          <p className="error">{errors.channel?.message}</p>
+        </div>
+
+        <button>Submit</button>
+
+        <DevTool control={control} />
+      </form>
+    </div>
+  );
+}
